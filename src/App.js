b4b20b0ee96823227c5183ca200ac62766302048
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2019-03-11 21:25:53 +0000

    making the containers toogle on different viewports

--- src/App.js
+++ src/App.js
@@ -5,14 +5,21 @@ import "./App.css";
 class App extends Component {
   constructor(props) {
     super(props);
+  //  this.handleContainer= this.handleContainer.bind(this);
     this.state = {
-      value: ""
+      value: "",
+      active: false
     };
   }
+//   handleContainer() {
+//     const currentState = this.state.active;
+//     this.setState({ active: !currentState });
+
+// };
 
   render() {
     return (
-      <div className="container  mb-5 ">
+      <div    className="container  mb-5 " id="mainContainer" >
         {" "}
         <Editor />
       </div>
--- src/components/Editor/Editor.js
+++ src/components/Editor/Editor.js
@@ -5,100 +5,164 @@ import Markdown from "react-markdown";
 class Editor extends Component {
   constructor(props) {
     super(props);
-console.log('props', props)
+    this.button = this.button.bind(this);
+    console.log("props", props);
     this.state = {
-      value: `# Welcome to my React Markdown Previewer!
+      value: ` # Welcome to my React Markdown Previewer!
 
-    ## This is a sub-heading...
-    ### And here's some other cool stuff:
+      ## This is a sub-heading...
+      ### And here's some other cool stuff:
+        
+     
       
-    Heres some code, <div></div>, between 2 backticks.
     
-  
-    // this is multi-line code:
-    
-    function anotherExample(firstLine, lastLine) {
-      if (firstLine == '' && lastLine == '') {
-        return multiLineCode;
-      }
-    }
- 
+        
+      You can also make text **bold**... whoa!
+      Or _italic_.
+      Or... wait for it... **_both!_**
+      And feel free to go crazy ~~crossing stuff out~~.
       
-    You can also make text **bold**... whoa!
-    Or _italic_.
-    Or... wait for it... **_both!_**
-    And feel free to go crazy ~~crossing stuff out~~.
-    
-    There's also [links](https://www.freecodecamp.com), and
-    > Block Quotes!
-    
-    And if you want to get really crazy, even tables:
-    
-    Wild Header | Crazy Header | Another Header?
-    ------------ | ------------- | ------------- 
-    Your content can | be here, and it | can be here....
-    And here. | Okay. | I think we get it.
-    
-    - And of course there are lists.
-      - Some are bulleted.
-         - With different indentation levels.
-            - That look like this.
-    
-    
-    1. And there are numbererd lists too.
-    1. Use just 1s if you want! 
-    1. But the list goes on...
-    - Even if you use drtyyyyyyyyyerashes or asterisks.
-    * And last but not least, let's not forget embedded images:
-    
-    ![React Logo w/ Text](https://goo.gl/Umyytc)
-    `
+      There's also [links](https://www.freecodecamp.com), and
+      > Block Quotes!
+      
+      And if you want to get really crazy, even tables:
+      
+      Wild Header | Crazy Header | Another Header?
+      ------------ | ------------- | ------------- 
+      Your content can | be here, and it | can be here....
+      And here. | Okay. | I think we get it.
+      
+      - And of course there are lists.
+        - Some are bulleted.
+           - With different indentation levels.
+              - That look like this.
+      
+      
+      1. And there are numbererd lists too.
+      1. Use just 1s if you want! 
+      1. But the list goes on...
+      - Even if you use dashes or asterisks.
+      * And last but not least, let's not forget embedded images:
+      
+      ![React Logo w/ Text](https://goo.gl/Umyytc)
+      [React Logo w/ Text](https://goo.gl/Umyytc)
+    `,
+      active: false,
+      fromWhere: ""
     };
   }
+  //   handleContainer() {
+  //     const currentState = this.state.active;
+  //     this.setState({ active: !currentState });
 
+  // };
   onSearchChange(event) {
-    
     this.setState({ value: event.target.value });
-  //  this.props.textMarked(event.target.value);
+    //  this.props.textMarked(event.target.value);
   }
-
-  render() {
-    const { value } = this.state;
+  containerSizeHandler() {
+    var element = document.querySelector(".editorContainer");
+    element.classList.toggle("maximized");
+    var element1 = document.getElementById("maxEditor");
+    element1.classList.toggle("fa-location-arrow");
+    var element3 = document.getElementById("previewContainer");
+    element3.classList.toggle("d-none");
+    var element4 = document.getElementById("mainContainer");
+    element4.classList.toggle("container");
+    var element5 = document.getElementById("maxEditor");
+    element5.classList.toggle("resizer");
+  }
+  containerSizeHandler2() {
+    var element = document.getElementById("previewContainer");
+    element.classList.toggle("container");
+    var element1 = document.getElementById("maxPreviwer");
+    element1.classList.toggle("fa-location-arrow");
+    var element3 = document.querySelector(".editorContainer");
+    element3.classList.toggle("d-none");
+    var element4 = document.getElementById("mainContainer");
+    element4.classList.toggle("container");
+    var element5 = document.getElementById("maxPreviwer");
+    element5.classList.toggle("resizer");
+  }
+  button(value) {
+    this.setState({ fromWhere: value });
+    console.log("this.value", this.state.fromWhere);
+  }
+  renderButton(action, style) {
+    return <i onClick={() => this.button(action)} className={style} />;
+  }
+  renderEditor() {
+    const { value, fromWhere } = this.state;
+    // if (fromWhere == "maxPreviwer") {
+    //   return;
+    // }
     return (
-      <React.Fragment>
-        <div className="mx-auto    col-xs-8 col-sm-8 col-md-8">
-          <div className="form-group">
-            <div className="toolbar editorWrap mt-5 ">
-              <i className=" pl-1 pr-2 fab fa-free-code-camp" />
-              Editor
-              <i className="fas fa-arrows-alt" />
-            </div>
-            <textarea
-         
-              onChange={(event)=>this.onSearchChange(event)}
-              className="w-100"
-              cols="10"
-              rows="10"
-              charswidth="23"
-              name="text_body"
-              id="editor"
-              value={value}
-            />
+      <div className=" mx-auto col-xs-8 col-sm-8 col-md-8">
+        <div className="form-group">
+          <div className="toolbar editorWrap mt-5 ">
+            <i className=" pl-1 pr-2 fab fa-free-code-camp" />
+            Editor
+            {fromWhere == ""
+              ? this.renderButton("maxEditor", "fas fa-arrows-alt")
+              : this.renderButton("", "fas fa-location-arrow")}
           </div>
+          <textarea
+            onChange={event => this.onSearchChange(event)}
+            className="w-100 h-100 editorContainer"
+            charswidth="23"
+            name="text_body"
+            id="editor"
+            value={value}
+          />
         </div>
-        <div className="   col-xs-12 col-sm-12 col-md-12">
-          <div className="previewWrap   preview">
-            <div className="toolbar   w-100">
-              <i className="pl-1 pr-2 fab fa-free-code-camp"> Previwer</i>
-              <i className="fas fa-arrows-alt" />
-            </div>
-            <div className="ml-3 mr-1" id="preview">
-              <Markdown source={value} />
-            </div>
-          </div>
-        </div>
-      </React.Fragment>
+      </div>
     );
+  }
+  renderPreviwer() {
+    const { value, fromWhere } = this.state;
+    // if (fromWhere == "maxEditor") {
+    //   return;
+    // }
+
+    return (
+      <div id="previewContainer" className="   col-xs-12 col-sm-12 col-md-12">
+        <div className="previewWrap   preview">
+          <div className="toolbar   w-100">
+            <i className="pl-1 pr-2 fab fa-free-code-camp"> Previwer</i>
+            {fromWhere == ""
+              ? this.renderButton("maxPreviwer", "fas fa-arrows-alt")
+              : this.renderButton("", "fas fa-location-arrow")}
+          </div>
+          <div className=" mr-1" id="preview">
+            <Markdown source={value} />
+          </div>
+        </div>
+      </div>
+    );
+  }
+  render() {
+    const { value, fromWhere } = this.state;
+    if (fromWhere == "maxEditor") {
+      return this.renderEditor();
+    } else if (fromWhere == "maxPreviwer") {
+      return this.renderPreviwer();
+    } else {
+      return (
+        <React.Fragment>
+          {this.renderEditor()}
+          {this.renderPreviwer()}
+        </React.Fragment>
+      );
+    }
+
+    // return (
+    //   fromWhere == "maxEditor" ?  this.renderPreviwer():  fromWhere == "maxPreviwer"? this.renderPreviwer():
+    //   <React.Fragment>
+
+    //     {this.renderEditor()}
+    //     {this.renderPreviwer()}
+    //   </React.Fragment>
+    // );
   }
 }
 
